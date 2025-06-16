8c90d8ce409f82d0f5653bb181dd49d51b07a045
Marcelo Henrique <marcelohenriquea.moura@outlook.com>
2025-06-16 18:49:34 -0300

    Implementado layout da tela e aplicação do CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [activeTab, setActiveTab] = useState('ask-ai')
+
+  const handleTabChange = (tab) => {
+    //alert(tab);
+    setActiveTab(tab);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        className={activeTab === 'ask-ai' ? 'active' : ''}
+        onClick={() => handleTabChange('ask-ai')}>
+        Talk with AI
+      </button>
+      <button
+        className={activeTab === 'recipe-generator' ? 'active' : ''}
+        onClick={() => handleTabChange('recipe-generator')}>
+        Generate Recipes
+      </button>
+      <button
+        className={activeTab === 'image-generator' ? 'active' : ''}
+        onClick={() => handleTabChange('image-generator')}>
+        Generate Images
+      </button>
+      <div>
+        {activeTab === 'ask-ai' && <h2>Talk with AI</h2>}
+        {activeTab === 'recipe-generator' && <h2>Generate Recipes</h2>}
+        {activeTab === 'image-generator' && <h2>Generate Images</h2>}
+      </div>
     </div>
   );
 }
