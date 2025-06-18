ea528c4942095128ff97bb7e3c1a60c5ace7f302
Marcelo Henrique <marcelohenriquea.moura@outlook.com>
2025-06-17 21:16:16 -0300

    Integração do layout do componente de receitas com a API

--- src/pages/recipe/RecipeGenerator.js
+++ src/pages/recipe/RecipeGenerator.js
@@ -1,9 +1,60 @@
-import react from "react";
+import react, { useState } from "react";
+import api from "../../services/api";
 
 function RecipeGenerator(){
+
+    const [ingredients, setIngredients] = useState('');
+    const [cuisine, setCuisine] = useState('Any');
+    const [dietaryRestrictions, setDietaryRestrictions] = useState('');
+    const [recipe, setRecipe] = useState('')
+
+    const createRecipe = async () => {
+        try {
+            const response = await  api.get(`recipe-creator`, {
+                params: {
+                    ingredients, 
+                    cuisine, 
+                    dietaryRestrictions
+                }
+            })
+            const data = await response.data;
+            console.log(data);
+            setRecipe(data);
+        } catch (error) {
+            console.log('Error generate recipe: ', error)
+        }
+    };
+
     return (
         <div>
             <h2>Generate Recipes</h2>
+            <input
+                type="text"
+                value={ingredients}
+                onChange={(e) => setIngredients(e.target.value)}
+                placeholder="Enter a ingredients (comma separated)"
+            />
+
+            <input
+                type="text"
+                value={cuisine}
+                onChange={(e) => setCuisine(e.target.value)}
+                placeholder="Enter cuisine type"
+            />
+
+            <input
+                type="text"
+                value={dietaryRestrictions}
+                onChange={(e) => setDietaryRestrictions(e.target.value)}
+                placeholder="Enter dietary restrictions"
+            />
+
+            <button onClick={createRecipe}>
+                Ask AI
+            </button>
+            <div className="output">
+                <p>{recipe}</p>
+            </div>
         </div>    
     );
 }
